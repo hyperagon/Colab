5ba72e1886758dabc94ef1c7b9c694118611ce00
Hyperag⬡n <23475800+hyperagon@users.noreply.github.com>
2025-10-20 12:50:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ if [ ! -d "cache" ]; then
     mkdir "cache"
 fi
 source sourceme
-PIP_CACHE_DIR=cache;pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-$PIP_CACHE_DIR=cache;pip3 install notebook
+PIP_CACHE_DIR="cache" pip3 --cache-dir="cache" install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+PIP_CACHE_DIR="cache" pip3 --cache-dir="cache" install notebook
 jupyter notebook
